9c75f0e40ce0bf7b32730c1ed15699cad74cd9eb
JLER2003 <134630238+JLER2003@users.noreply.github.com>
2023-09-20 23:05:20 -0500

    Update settings.py

--- market/market/settings.py
+++ market/market/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'U8e3Rn6dXk29J4pdKhe0',
+        'HOST': 'containers-us-west-47.railway.app',  
+        'PORT': '7164',   
     }
 }
 
